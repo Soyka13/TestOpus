ea6423ea468276ebf47489009ce45d53f1826fc6
Soyka13 <helen.stepaniuk@gmail.com>
2024-02-07 10:48:44 +0200

    Update failure_expected

--- testopus/utils/failure_expected_decorator.py
+++ testopus/utils/failure_expected_decorator.py
@@ -9,13 +9,15 @@ class FailureExpectedException(Exception):
 
 def failure_expected(func):
     """
-    Decorator that raises a FailureExpectedException if an exception is raised during function execution.
+    Decorator that raises a FailureExpectedException with error message if any occurred else empty string.
     Use this decorator when you expect failure in your test.
     """
     def wrapper(*args, **kwargs):
+        error_string = ""
         try:
             func(*args, **kwargs)
         except Exception as e:
             error_string = f"{type(e).__name__}: {e}"
-            raise FailureExpectedException(message=error_string)
+
+        raise FailureExpectedException(message=error_string)
     return wrapper
